b72f0c4c56d7a17483e3e3a12091bd9d5f92de9a
PrussianDachshund <michal.banach98@wp.pl>
2023-04-09 14:31:08 +0200

    v.0.4

--- src/main/java/com/example/po_projekt1/HelloController.java
+++ src/main/java/com/example/po_projekt1/HelloController.java
@@ -1,11 +1,18 @@
 package com.example.po_projekt1;
 
+import javafx.geometry.Insets;
+import jfxtras.styles.jmetro.JMetro;
+import jfxtras.styles.jmetro.Style;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private Label home_pict;
 
     @FXML
     protected void onHelloButtonClick() {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.po_projekt1 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.jfxtras.styles.jmetro;
 
 
     opens com.example.po_projekt1 to javafx.fxml;
